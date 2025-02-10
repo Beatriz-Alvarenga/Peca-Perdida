bb913a8bab5b50502f9a6cad7ce1997fa1f7dc5e
Beatriz Alvarenga <123256926+Beatriz-Alvarenga@users.noreply.github.com>
2025-02-10 00:30:15 -0300

    Trabalho Final QUEBRA-CABEÇA
    :)

--- Trabalho Final QUEBRA-CABEÇA.c
+++ Trabalho Final QUEBRA-CABEÇA.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
+int scanSomatoriaPecas(int Nqb) {
+    int somaP = 0, // declaração da variável somaP (Soma das Peças)
+        Npecas = 0; // declaração da variável Npecas (Número de Peças)
+        i = 0; // declaração da variável i (Iterador)
+    for(i = 0; i < Nqb - 1 ; i++){ // Laço de repetição para receber os números das peças que temos (Nqb - 1)
+        scanf("%d", &Npecas); // Recebendo os valores das peças que temos (de 1 até Nqb - 1)
+        somaP = somaP + Npecas; // Somando os números de peças que temos
+    }
+    return somaP; // Retorna a somatória
+}
+
 int main() {
     
     int Nqb = 0, // declaração da variável N (Número do Quebra-cabeça)
-    i = 0, // declaração da variável i (Iterador)
-    Npecas = 0, // declaração da variável Npecas (Número de Peças)
     somaP = 0, // declaração da variável somaP (Soma das Peças)
     somaN = 0, // declaração da variável somaN (Soma de Números)
     diff = 0; // declaração da variável diff (Diferença)
     
     scanf("%d", &Nqb); // Recebendo o valor do Quebra-Cabeça
     
-    for(i = 0; i < Nqb - 1 ; i++){ // Laço de repetição para receber os números das peças que temos (Nqb - 1)
-        scanf("%d", &Npecas); // Recebendo os valores das peças que temos (de 1 até Nqb - 1)
-        somaP = somaP + Npecas; // Somando os números de peças que temos
-    }
+    somaP = scanSomatoriaPecas(Nqb); // Recebendo valores da função de leitura
     
     for(; Nqb > 0; Nqb--){ // Usando Nqb como parâmetro para o limite de parada do laço for (Decrementando de 1 em 1)
         somaN = somaN + Nqb; // Somando o decremento da variável Nqb de 1 em 1 (EX: 5 + 4 + 3 + 2 + 1 = 15)
